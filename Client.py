2dac6b95663be5da5d290094cf31528b950aa482
Martin Møller <mrfm16@student.aau.dk>
2018-11-07 13:40:52 +0100

    client/server

--- Client.py
+++ Client.py
@@ -1,9 +1,29 @@
 import socket                   # Import socket module
 import cv2
+
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
-port = 12345                    # Reserve a port for your service.
+port = 60000                    # Reserve a port for your service.
 
 s.connect((host, port))
-print(s.recv(1024))
-s.close                         # Close the socket when done
+k = 'hello server!'
+s.send(k.encode())
+
+with open('test_file', 'wb') as f: #change test_File to desired name of the file
+    print('file opened')
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
+
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
+img = cv2.imread("test_file")
+cv2.imshow("test", img)
+cv2.waitKey()
--- Server.py
+++ Server.py
@@ -1,14 +1,28 @@
-import socket                 # Import socket module
+import socket                   # Import socket module
 
-s = socket.socket()           # Create a socket object
-host = socket.gethostname()   # Get local machine name
-port = 12345                  # Reserve a port for your service.
-s.bind((host, port))          # Bind to the port
+port = 60000                    # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+s.bind((host, port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
 
-s.listen(5)                   # Now wait for client connection.
+print('Server listening....')
+a = "Thank you for connecting"
 while True:
-   c, addr = s.accept()       # Establish connection with client.
-   print('Got connection from', addr)
-   k = 'test'
-   c.send(k.encode())
-   c.close()                  # Close the connection
+    conn, addr = s.accept()     # Establish connection with client.
+    print('Got connection from', addr)
+    data = conn.recv(1024)
+    print('Server received', repr(data))
+
+    filename='book.jpg'         #change this variable for whatever file you want to transfer
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',repr(l))
+       l = f.read(1024)
+    f.close()
+
+    print('Done sending')
+    conn.send(a.encode())
+    conn.close()
