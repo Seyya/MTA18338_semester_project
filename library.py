fbe5a5406216fe1f4f3972e969d47cfb4d6ce11f
abs <abschm@users.noreply.github.com>
2018-11-09 13:17:17 +0100

    Redid rgb2grey to use numpy and is much faster now + deleted all 'video' functions

--- library.py
+++ library.py
@@ -1,10 +1,8 @@
-import time
 import cv2
 import numpy as np
 
 
 def binary_threshold(img, threshold):
-    # original code: 85.5 ns
     # original code w farmhouse img: 1.1 s
     # modified w farmhouse: 76.1 ms
     img[img > threshold] = 255
@@ -22,8 +20,7 @@ def binary_threshold(img, threshold):
 
 
 def adaptive_thresholding(img):
-    #original code w farmhouse resized to 200x200:
-    img = cv2.resize(img, (200, 200))   # brug resize hvis billedet er større end 500x500
+    img = cv2.resize(img, (200, 200))  # brug resize hvis billedet er større end 500x500
     height, width = img.shape
     img_out = img.copy()
 
@@ -44,60 +41,28 @@ def adaptive_thresholding(img):
                 b = 0
             img_out.itemset((i, j), b)  # apply the changes threshold changes to the image
             adapt_thr = img_out.copy()
-            #print("pos", i, j)
+            # print("pos", i, j)
     return adapt_thr
 
 
-def adaptive_thresholding2video(frame_gray):  # needs greyscale frame to work
-    # frame_resize = cv2.resize(img, (200, 200))   # brug resize hvis den skal opdater hurtigere, og ændre frame variable til resize variable
-    height, width = frame_gray.shape[:2]
-    frame_out = np.zeros((height, width), np.uint8)
+def rgb2grey(img):
+    # original code on farmhouse img: a long long long time
+    # modified code -//-: 192 ms
+    grey_val = 0.07 * img[:, :, 2] + 0.72 * img[:, :, 1] + 0.21 * img[:, :, 0]
+    grey_img = grey_val.astype(np.uint8)
+    return grey_img
 
-    for i in range(1, height - 7):  # loop that goes through the height of the image with offset 1
-        for j in range(1, width - 7):  # Loop that goes through the width of the image with offset 1
-            sum = 0
-            pix_value = frame_gray.item(i, j)  # gets the value of a certain pixel at height/width
-
-            for k in range(-11, 2):  # loop that goes through the pixels in a 13x13 of the pixel at height/width
-                for l in range(-11, 2):  # loop that goes through the pixels in a 13x13 of the pixel at height/width
-                    neighbor_pixels = frame_gray.item(i + k, j + l)  # Get the value of the pixels in a 3x3 shape
-                    sum = sum + neighbor_pixels
-
-            b = sum / 169  # sum of all neighbourhood pixel values divided by amount of pixels to get average pixel value
-            if pix_value > b:  # compares pixvalue with mean value to set it to black or white
-                b = 255
-            else:
-                b = 0
-            frame_out.itemset((i, j), b)  # apply the changes threshold changes to the image
-            adapt_thr = frame_out.copy()
-            print("pos", i, j)
-    return adapt_thr
+    # h, w, _ = img.shape
+    #
+    # gray = np.zeros((h, w), np.uint8)
+    # for i in range(h):
+    #     for j in range(w):
+    #         gray[i, j] = np.clip(0.07 * img[i, j, 0] + 0.72 * img[i, j, 1] + 0.21 * img[i, j, 2], 0, 255)
+    #
+    # cv2.imshow("gray", gray)
 
 
-def rgb2grey2image(img):
-    h, w = img
-    ts = time.time()
-
-    gray = np.zeros((h, w), np.uint8)
-    for i in range(h):
-        for j in range(w):
-            gray[i, j] = np.clip(0.07 * img[i, j, 0] + 0.72 * img[i, j, 1] + 0.21 * img[i, j, 2], 0, 255)
-
-    t = (time.time() - ts)
-    cv2.imshow("gray", gray)
-
-
-def rgb2grey2video(frame):
-    h, w = frame.shape[:2]
-    gray = np.zeros((h, w), np.uint8)
-    for i in range(h):
-        for j in range(w):
-            gray[i, j] = np.clip(0.07 * frame[i, j, 0] + 0.72 * frame[i, j, 1] + 0.21 * frame[i, j, 2], 0, 255)
-
-    return gray
-
-
-def dilateboi(img_arr, iteration):
+def dilate(img_arr, iteration):
     h, w = img_arr.shape
     it = 0
     img_new = img_arr.copy()
@@ -119,12 +84,12 @@ def dilateboi(img_arr, iteration):
     return img_new
 
 
-def erodeboi(img_arr, iteration):
+def erode(img_arr, iteration):
     h, w = img_arr.shape
     it = 0
     img_new = img_arr.copy()
     while it != iteration:
-        print("Erotion iteration: " + str(it + 1))
+        print("Erosion iteration: " + str(it + 1))
         for j in range(1, w - 1):
             for i in range(1, h - 1):
                 if img_arr[i, j] == 0:
