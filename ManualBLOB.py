4993197b0894280136b274913273a5708907c4c7
Laerke91 <36537016+Laerke91@users.noreply.github.com>
2018-11-13 10:46:06 +0100

    Fixed the while loop - the out of bounds issue with the grassfire algorithm is still there

--- ManualBLOB.py
+++ ManualBLOB.py
@@ -2,46 +2,52 @@ import numpy as np
 import cv2
 
 #Open image, convert to greyscale and blur
-testImg = cv2.imread('TestSpots.jpg', 0)
+testImg = cv2.imread('Sheeps01.jpg', 0)
 blur = cv2.GaussianBlur(testImg,(5,5),0)
 __, binaryImg = cv2.threshold(testImg,127,255,cv2.THRESH_BINARY)
+#cam = cv2.VideoCapture(0)
+#ret, frame = cam.read()
+#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
-def runGrassFire(id, location, treshVal, sourceImg, result):
+def runGrassFire(id, location, treshMin, treshMax, sourceImg, result):
     height = sourceImg.shape[0]
     width = sourceImg.shape[1]
     #Create array for new locations to check
     newQueue = []
-    print("The location is: " + str(location))
+    #print("The location is: " + str(location))
+
     #Check pixel above
     y, x = location
 
     if y - 1 >= 0:
         if result[y -1, x] == 0:
             #If the pixel is within the treshold, assign ID and add to queue
-            if sourceImg[y -1, x] <= treshVal:
-                print("Found pixel above.")
+            if sourceImg[y -1, x] >= treshMin and sourceImg[y - 1, x] <= treshMax:
+                #print("Found pixel above.")
+
                 result[y -1, x] = id
                 newPosition = (y - 1, x)
                 #Append the new location coordinates to the queue
                 newQueue.append(newPosition)
 
     #Check pixel to the right
-    if x + 1 <= width:
+    if x + 1 < width:
         if result[y, x + 1] == 0:
-            if sourceImg[y, x +1] <= treshVal:
-                print("Found pixel to the right")
+            if sourceImg[y, x +1] >= treshMin and sourceImg[y, x + 1] <= treshMax:
+                #print("Found pixel to the right")
+
                 result[y, x +1] = id
                 newPosition = (y, x +1)
                 newQueue.append(newPosition)
 
     #Check pixel below
-    if y + 1 <= height:
+    if y + 1 < height:
         #print("The y-coordinate is: " + str(y))
         if result[y + 1, x] == 0:
             #print("The x and y coordinates are: " + str(y, x))
-            if sourceImg[y + 1, x] <= treshVal:
-                print("Found pixel below")
+            if sourceImg[y + 1, x] >= treshMin and sourceImg[y + 1, x] <= treshMax:
+                #print("Found pixel below")
+
                 result[y + 1, x] = id
                 newPosition = (y + 1, x)
                 newQueue.append(newPosition)
@@ -49,16 +55,18 @@ def runGrassFire(id, location, treshVal, sourceImg, result):
     #check pixel to the left
     if x -1 >= 0:
         if result[y, x -1] == 0:
-            if sourceImg[y, x -1] <= treshVal:
-                print("Found pixel to the left")
+            if sourceImg[y, x -1] >= treshMin and sourceImg[y, x - 1] <= treshMax:
+                #print("Found pixel to the left")
+
                 result[y, x -1] = id
                 newPosition = (y, x -1)
                 newQueue.append(newPosition)
-    print("The new queue is: " + str(newQueue))
+
+    #print("The new queue is: " + str(newQueue))
     return result, newQueue
 
 
-def findBlob(sourceImg, treshVal):
+def findBlob(sourceImg, treshMin, treshMax):
     #Create nesesary variables
     height = sourceImg.shape[0]
     width = sourceImg.shape[1]
@@ -67,47 +75,42 @@ def findBlob(sourceImg, treshVal):
     result = np.zeros(sourceImg.shape)
 
     #Loop through each pixel in the image
-    for Ypos in range(0, width -1):
-        for Xpos in range(0, height -1):
+    for Ypos in range(0, width-1):
+        for Xpos in range(0, height-1):
             #If the value of x and y is 0..
-            if result[Ypos, Xpos] == 0:
-                print(Xpos)
-                print(Ypos)
+            if result[Xpos, Ypos] == 0:
                 #.. check treshold..
-                if sourceImg[Ypos, Xpos] <= treshVal:
+                if sourceImg[Xpos, Ypos] >= treshMin and sourceImg[Xpos, Ypos] <= treshMax:
                     #.. and assign an id before running the grass-fire algorithm
                     #Overwrite the result and add the coordinates to the queue
-                    sourceImg[Ypos, Xpos] = blobID
-                    position = (Ypos, Xpos)
-                    print("The position is: " + str(position))
-                    newResult, newQueue = runGrassFire(blobID, position, treshVal, sourceImg, result)
+                    sourceImg[Xpos, Ypos] = blobID
+                    position = (Xpos, Ypos)
+                    #print("The position is: " + str(position))
+                    newResult, newQueue = runGrassFire(blobID, position, treshMin, treshMax, sourceImg, result)
                     result = newResult
                     queue.extend(newQueue)
-                    print("The current BLOB id is: " + str(blobID))
+                    #print("The current BLOB id is: " + str(blobID))
+
                     #As long as there is coordinates in the queue, keep running the grass-fire algorithm
                     while len(queue) != 0:
-                        print("The length of the queue is: " + str(len(queue)))
-                        print("This is the current queue: " + str(queue))
-                        print("The first element of the queue is: " + str(queue[0]))
+                        #print("The length of the queue is: " + str(len(queue)))
+                        #print("This is the current queue: " + str(queue))
+                        #print("The first element of the queue is: " + str(queue[0]))
+
                         #Take the first entry in the queue, run algorithm ect. before deleting the first entry in the queue
                         firstInQueue = queue[0]
-
-                        newResult, newQueue = runGrassFire(blobID, firstInQueue, treshVal, sourceImg, result)
+                        newResult, newQueue = runGrassFire(blobID, firstInQueue, treshMin, treshMax, sourceImg, result)
                         result = newResult
                         queue.extend(newQueue)
-                        #queue.pop(0)
                         del queue[0]
-
-                        print(str(len(queue)))
+                        #print(str(len(queue)))
                     blobID += 1
     return result
 
 
-a = findBlob(blur, 175)
+a = findBlob(testImg, 0, 50)
 cv2.imshow('Whatever', a)
-
-#Show the binary image
-#cv2.imshow('TEEEEST', runThroughImg(blur, 150))
-cv2.imshow('Binary', binaryImg)
+cv2.imshow('Original', testImg)
+#cv2.imshow('Feed', frame)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
